c0daec799800e316632d17c0c679bc32e66d7ec3
Nicolas Ruge <nicolasruge.13@gmail.com>
2024-06-18 23:06:17 -0500

    Creacion encarpetado

---
+++ src/main/java/com/alma/alma/AlmaApplication.java
@@ -0,0 +1,13 @@
+package com.alma.alma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alma/alma/AlmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alma.alma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
